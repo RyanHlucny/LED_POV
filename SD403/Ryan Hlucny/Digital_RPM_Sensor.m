8a323ebd1f6d339ce625d5e55167e1606ad127c8
RyanHlucny <tools1157@gmail.com>
2024-12-18 01:56:40 -0600

    Added new digital rpm sensor matlab script and restored a picture

--- SD403/Ryan Hlucny/Digital_RPM_Sensor.m
+++ SD403/Ryan Hlucny/Digital_RPM_Sensor.m
@@ -1,9 +1,9 @@
 %% Digital RPM Sensor Algorithm
 % Creating artificial test data
 
-Fs = 1e3;
+Fs = 1e4;
 dt = 1/Fs;
-t_end = 2;
+t_end = 10;
 t = 0:dt:t_end-dt;
 
 % Data will be 16-bit unsigned ADC values (0-65535)
@@ -11,54 +11,72 @@ a = 10e3;
 base = 30e3;
 f = 5;
 noise_std = 1e3;
-data = a*sin(2*pi*f*t)+base;
+data = a*(sin(2*pi*f/2*t).^2-0.5*sin(4*pi*f/2*t).^2)+base;
 data_noise = data + noise_std*randn(size(t));
 
 figure; tiledlayout('vertical'); nexttile;
-plot(t, data);
+plot(t, data); title('Data without Noise'); xlabel('time');
 nexttile;
 plot(t, data_noise);
 
 %% Moving average envelope
 avg = zeros(size(t));
 trigger = zeros(size(t));
-upper_thresh = 2;
-lower_thresh = 1.5;
-upper = zeros(size(t));
-lower = zeros(size(t));
-test = zeros(size(t));
-thresh = 0;
-W = 300;
+f = zeros(size(t));
+state = 0;
+W = 2000;
+rising_edge = 0;
+prev_rising_edge = 0;
+up_thresh = ones(size(t))*0.1*65535;
+down_thresh = -ones(size(t))*0.01*65535;
 for i = 1:numel(t)
-    if i < W
-        continue;
-    end
-
-    avg(i) = sum(abs(data_noise(i-W+1:i)))/W;
-    
-    upper(i) = (upper_thresh)*avg(i);
-    lower(i) = (lower_thresh)*avg(i);
-    test(i) = abs(data_noise(i)-avg(i));
-    if abs(data_noise(i)-avg(i)) > upper_thresh*avg(i)
-        thresh = 1;
-    elseif abs(data_noise(i)-avg(i)) < lower_thresh*avg(i)
-        thresh = -1;
-    end
-
-    if thresh == 1 
-        trigger(i) = 1;
-    elseif thresh == -1
-        trigger(i) = -1;
+    if i < 2
+        avg(i) = data_noise(i);
+        % continue;
+    elseif i <= W
+        avg(i) = avg(i-1)*(i-1)/i + data_noise(i)/i;
     else
-        trigger(i) = 0;
+        avg(i) = avg(i-1) + (data_noise(i)-data_noise(i-W))/W;
+        % sum(data_noise(i-W+1:i))/W;
     end
+    
+    DC = data_noise(i) - avg(i);
+
+    % State variable control
+    if state == 0
+        if DC > up_thresh(i)
+            state = 1;
+            rising_edge = t(i);
+            % f calculation
+            period_sec = rising_edge - prev_rising_edge;
+            freq = 1/period_sec;
+            f(i) = freq;
+        end
+    end
+    if state == 1
+        if DC < down_thresh(i)
+            state = 0;
+        end
+    end
+
+    % State machine output
+    if state == 0
+        trigger(i) = 0;
+    elseif state == 1
+        trigger(i) = 1;
+    end
+
+    prev_rising_edge = rising_edge;
 end
 
 hold on;
-plot(t, avg);
-plot(t,upper);
-plot(t,lower);
-plot(t,test);
-legend('Data', 'avg', 'upper', 'lower', 'abs diff')
+plot(t, avg); grid on; title('Signal with Noise and Moving Average'); xlabel('time');
 nexttile;
-plot(t,trigger);
+DC_shift = data_noise-avg;
+plot(t,DC_shift); grid on; title('DC Shifted Signal'); xlabel('time');
+hold on;
+plot(t, up_thresh); plot(t, down_thresh); legend('Signal','Up Thresh','Down Thresh');
+nexttile;
+plot(t,trigger); grid on; title('Trigger'); xlabel('time');
+nexttile;
+plot(t(f~=0),f(f~=0)); grid on; ylabel('Hz'); xlabel('time');
